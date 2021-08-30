eb7697673f16ee90712aecd76c1e0cc320c5efbc
5hel6y <89475726+5hel6y@users.noreply.github.com>
2021-08-30 17:41:28 +0300

    Update HW2.0.py
    Так как в цикл рандом я не включил выполнял он его каждый раз одинаково исправил

--- HW2.0.py
+++ HW2.0.py
@@ -2,14 +2,12 @@ import random
 balanse = 10000
 s = 1000
 print("Здравствуйте вы попали в виртуальное казино с прекрасным названием HW2.0")
-limit = 12
-randomnumb = random.randint(2, limit)
 print("Ваш стартовый дипозит 10000 рублей \nПравила такие играем в угадай число ставка 1000 руб0лей\nКрупье бросает кости максимум может выпасть 12 минимум 2 при совпадении + 1000 не угадал ставка сгорает\nНадеюсь все понятно")
 while True :
   number = ("Загадывай число")
   print(number)
   number = int(input())
-  
+  randomnumb = random.randint(2, 13)
   print("Вы загадали цифру", number)
   print("Число которое выпало у крупье равно", randomnumb)
   if randomnumb == int(number):
